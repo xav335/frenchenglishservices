56ccdc41a6e3f4727b08ac604906882ee680e63f
kimsufi <root@ks394808.kimsufi.com>
2015-02-24 08:40:27 +0100

    Sauvegarde juste APRES MAJ 4.1.1 + iconeo

--- wp-content/themes/twentyfourteen/footer.php
+++ wp-content/themes/twentyfourteen/footer.php
@@ -18,7 +18,7 @@
 
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
+				<a href="http://www.iconeo.fr" target="_blank">Powered by Iconeo.fr</a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
